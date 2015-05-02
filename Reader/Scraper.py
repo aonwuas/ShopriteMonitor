5ccb543b60f2c1412e836496cf55a728ed241fea
Anthony O <ieatbeens@gmail.com>
2015-05-02 19:35:38 -0400

    Scraper and formatter
    Added a string formatter
    Scraper near complete
    TODO:
    Grab store info in scraper
    Figure out why some prices/names have html in them

--- Reader/Scraper.py
+++ Reader/Scraper.py
@@ -1,13 +1,13 @@
 import urllib2
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # , UnicodeDammit
 from datetime import datetime
 from models import Circular, Item
+import stringformatter as fmt
 
 
 class Scraper:
     def __init__(self, url):
         self.link = url  # url for Categories page of store
-        self.base_url = "http://plan.shoprite.com"
         
     @staticmethod
     def full_url(link):
@@ -15,20 +15,21 @@ class Scraper:
     
     @staticmethod
     def read_page(link):
-        req = urllib2.Request(Scraper.full_url(link))
+        req = urllib2.Request(link)
         req.add_header('User-agent', 'Mozilla/5.0')
+
         return BeautifulSoup(urllib2.urlopen(req).read())
 
     def scrape_category_data(self):
         category_page = self.read_page(Scraper.full_url(self.link))
         c = Circular()
-        c.store = "Shop Rite Newton"
+        # c.store = "Shop Rite Newton"
         c.start = c.end = datetime.now()
         for link in self.find_tag(category_page, "a", "data-clientanalyticsaction", "Circular Categories"):
             cat_link = link.get("href")
             cat_name = link.get("data-clientanalyticslabel")
             cat_name = cat_name[:cat_name.rfind(" - ")]
-            Scraper.read_category(cat_link, cat_name)
+            Scraper.read_category(Scraper.full_url(cat_link), cat_name)
 
     @staticmethod
     def find_tag(page, tag, var=None, val=None):
@@ -38,16 +39,23 @@ class Scraper:
             return page.find_all(tag)
 
     @staticmethod
-    def create_item(list_item, cat_name):
-        new_item = Item()
-        new_item.name = list_item.find_all("h3", {"class": "circular-item-title"})[0].contents[0]
-        new_item.category = cat_name
-        new_item.price = list_item.find_all("h4", {"class": "circular-item-price"})[0].contents[0]
-        new_item.save()
+    def create_item(div, name):
+        i = Item()
+        i.name = fmt.convert(div.find("h2").contents)
+        i.price = fmt.convert(div.find("p").contents)
+        i.category = name
+        i.save()
 
     @staticmethod
     def read_category(link, name):
         page = Scraper.read_page(link)
-        for li in Scraper.find_tag(page, "li"):
-            Scraper.create_item(li, name)
-        next_page = page.find_all("a", {"data-role": "button"})[0].get("href")
+        # Grab div for inventory items and pass to Item creator
+        for div in Scraper.find_tag(page, "div", "class", "grid-item"):
+            Scraper.create_item(div, name)
+        # Check for additional pages
+        for a in Scraper.find_tag(page, "a", "title", "Next Page"):
+            n_page = Scraper.full_url(a.get("href"))
+            if n_page == link:
+                return True
+            else:
+                return Scraper.read_category(n_page, name)
---
+++ Reader/migrations/0001_initial.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Circular',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start', models.DateField()),
+                ('end', models.DateField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Item',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('category', models.CharField(max_length=255)),
+                ('price', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Sale',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('item', models.ForeignKey(to='Reader.Item')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Store',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('address', models.CharField(max_length=255)),
+                ('phone_number', models.CharField(max_length=40)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='circular',
+            name='store',
+            field=models.ForeignKey(to='Reader.Store'),
+        ),
+    ]
