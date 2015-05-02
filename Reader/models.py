110a6540701a206ad78d79ea3cff1dc3fac65654
Anthony O <ieatbeens@gmail.com>
2015-05-01 20:31:19 -0400

    Added Reader App

---
+++ Reader/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=40)
+
+class Circular(models.Model):
+    store = models.ForeignKey(Store)
+    start = models.DateField()
+    end = models.DateField()
+
+class Item(models.Model):
+    circular = models.ForeignKey(Circular)
+    name = models.CharField(max_length=255)
+
+class Sale(models.Model):
+    item = models.ForeignKey(Item)
+    name = models.CharField(max_length=255)
